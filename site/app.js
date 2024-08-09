c960cbde7625c0cc0bdd248d73d9f7a7a6e365c3
Jonatas Barbosa <jonatascb@gmail.com>
2024-08-08 17:57:24 -0700

    Minor refactor

--- site/app.js
+++ site/app.js
@@ -1,21 +1,22 @@
-/**
- * Initializes the visit counter on the webpage.
- */
+const visitorCounterDigits = 6;
+
 const init = () => {
     const updatedVisitCounter = getUpdatedVisitCounter();
     const spanElements = getSpanElements(updatedVisitCounter);
     const counterElement = document.querySelector('.counter');
-    counterElement.innerHTML = spanElements;
+    if (counterElement) {
+        counterElement.innerHTML = spanElements;
+    }
 };
 
 const getUpdatedVisitCounter = () => {
-    const visitCounter = (Number(localStorage.getItem('visitCounter')) + 1) || 1;
+    const visitCounter = Number(localStorage.getItem('visitCounter')) + 1;
     localStorage.setItem('visitCounter', visitCounter);
     return visitCounter;
 }
 
 const getSpanElements = (visitCounter) => {
-    const visitCountStr = visitCounter.toString().padStart(6, '0');
+    const visitCountStr = visitCounter.toString().padStart(visitorCounterDigits, '0');
     const spanElements = visitCountStr.split('').map(digit => `<span>${digit}</span>`).join(' ');
     return spanElements;
 }
